e775d4a72ef3c835f15d1d9f8889bbaf48cd6668
Aowlyne <aowlyne@gmail.com>
2023-12-15 18:46:13 -0500

    install font awesone

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-app.use(router)
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-app.mount('#app')
+/* import all icons */
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(fas, far, fab);
+
+const app = createApp(App);
+
+app.use(router);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
