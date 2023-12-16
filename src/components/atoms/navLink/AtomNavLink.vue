98073a82e470c4bc66799b76d05673b5e6ef3032
Aowlyne <aowlyne@gmail.com>
2023-12-15 21:27:30 -0500

    create atom nav link

---
+++ src/components/atoms/navLink/AtomNavLink.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+export default {
+  name: "AtomNavLink",
+};
+</script>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+
+defineProps({
+  icon: String,
+  url: String,
+  text: String,
+  extraClasses: String,
+});
+
+let organismClass = "a-navLink";
+</script>
+
+<template>
+  <RouterLink :to="url">
+    <font-awesome-icon
+      :icon="['fas', icon]"
+      :class="[organismClass + '__icon', extraClasses]"
+    />
+    <span :class="[organismClass + '__text', extraClasses]">{{ text }}</span>
+  </RouterLink>
+</template>
+
+<style scoped lang="scss">
+@import "./navLink.scss";
+</style>
