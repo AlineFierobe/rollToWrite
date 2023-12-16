31a728b8ef7c220456d078e295248080d11d3a60
Aowlyne <aowlyne@gmail.com>
2023-12-16 11:53:01 -0500

    create atom link

---
+++ src/components/atoms/link/AtomLink.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+export default {
+  name: "Link",
+};
+</script>
+
+<script setup lang="ts">
+import FontAwesome from "../fontAwesome/AtomFontAwesome.vue";
+
+const props = defineProps<{
+  extraClasses?: string | Array<string>;
+  icon?: string;
+  iconBeforeText?: boolean;
+  iconSize?: string;
+  text: string;
+  url: string;
+  variant?: string;
+  version?: string;
+}>();
+
+let _atom_class = "a-link";
+let _version = props.version ? "-" + props.version : "-alpha";
+let _variant = props.variant ? "-" + props.variant : "-primary";
+</script>
+
+<template>
+  <a :href="url" :class="[_atom_class, _version, _variant, extraClasses]">
+    <FontAwesome
+      v-if="icon && iconBeforeText"
+      :icon="icon"
+      :size="iconSize"
+      :extraClasses="_atom_class + '__icon'"
+    />
+
+    <span>{{ text }}</span>
+
+    <FontAwesome
+      v-if="icon && !iconBeforeText"
+      :icon="icon"
+      :size="iconSize"
+      :extraClasses="_atom_class + '__icon'"
+    />
+  </a>
+</template>
+
+<style scoped lang="scss">
+@import "./_link";
+</style>
