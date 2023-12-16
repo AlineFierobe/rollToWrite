d0adbce2ee2e4587f3719652e8a4df4886934122
Aowlyne <aowlyne@gmail.com>
2023-12-16 00:08:56 -0500

    create atom font awesome

---
+++ src/components/atoms/fontAwesome/AtomFontAwesome.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+  name: "AtomFontAwesome",
+};
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+  icon: string;
+  size?: string;
+  variant?: string;
+  extraClasses?: Array<string>;
+}>();
+
+let _atom_class = "a-fontAwesome";
+let _size = props.size ? props.size : "1x";
+let _variant = props.variant ? props.variant : "fas";
+</script>
+
+<template>
+  <font-awesome-icon
+    :icon="[_variant, icon]"
+    :class="[_atom_class, extraClasses]"
+    :size="_size"
+  />
+</template>
+
+<style scoped lang="scss"></style>
