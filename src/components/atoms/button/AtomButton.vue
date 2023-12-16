9be69f038c2802f374deb7f3281afb1157ad5560
Aowlyne <aowlyne@gmail.com>
2023-12-16 01:15:22 -0500

    create atom button

---
+++ src/components/atoms/button/AtomButton.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+export default {
+  name: "Button",
+};
+</script>
+
+<script setup lang="ts">
+import FontAwesome from "../fontAwesome/AtomFontAwesome.vue";
+
+const props = defineProps<{
+  extraClasses?: string | Array<string>;
+  icon?: string;
+  iconBeforeText?: boolean;
+  iconSize?: string;
+  text: string;
+  variant?: string;
+  version?: string;
+}>();
+
+let _atom_class = "a-button";
+let _version = props.version ? "-" + props.version : "-alpha";
+let _variant = props.variant ? "-" + props.variant : "-primary";
+</script>
+
+<template>
+  <button :class="[_atom_class, _version, _variant, extraClasses]">
+    <FontAwesome
+      v-if="icon && iconBeforeText"
+      :icon="icon"
+      :size="iconSize"
+      :extraClasses="_atom_class + '__icon'"
+    />
+
+    <span>{{ text }}</span>
+
+    <FontAwesome
+      v-if="icon && !iconBeforeText"
+      :icon="icon"
+      :size="iconSize"
+      :extraClasses="_atom_class + '__icon'"
+    />
+  </button>
+</template>
+
+<style scoped lang="scss">
+@import "./_button";
+</style>
