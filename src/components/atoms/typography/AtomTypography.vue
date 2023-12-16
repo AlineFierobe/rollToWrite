2a2207fffa0cdf2b692fa2451d97c7fb1d2e97b9
Aowlyne <aowlyne@gmail.com>
2023-12-16 12:15:39 -0500

    create atom typography

---
+++ src/components/atoms/typography/AtomTypography.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+export default {
+  name: "Typography",
+};
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+  extraClasses?: string | Array<string>;
+  htmlTag?: string;
+  version?: string;
+  html: string;
+}>();
+
+let _atom_class = "a-typography";
+let _tag = props.htmlTag ? props.htmlTag : "div";
+let _version = props.version ? "-" + props.version : "-body1";
+</script>
+
+<template>
+  <component
+    :class="[_atom_class, _version, extraClasses]"
+    :is="_tag"
+    v-html="html"
+  />
+</template>
+
+<style scoped lang="scss">
+@import "./_typography";
+</style>
