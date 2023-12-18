311ad09a9d10ed49c3345d59a465799bf545465d
Aowlyne <aowlyne@gmail.com>
2023-12-18 18:06:59 -0500

    create Banner Alpha

---
+++ src/components/molecules/banner/bannerAlpha/MoleculeBannerAlpha.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  extraClasses?: string | Array<string>;
+  url: string;
+  alt: string;
+}>();
+
+let _molecule_class = "m-bannerAlpha";
+</script>
+
+<template>
+  <section
+    :alt="alt"
+    :class="[_molecule_class, extraClasses]"
+    :style="'background: url(' + url + ') center center/cover'"
+  ></section>
+</template>
+
+<style scoped lang="scss">
+@import "./_bannerAlpha";
+</style>
