227abc222e6b3ed6d75a8c510b5870b6a35410c8
Aowlyne <aowlyne@gmail.com>
2023-12-18 17:44:44 -0500

    create atom image

---
+++ src/components/atoms/image/AtomImage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  alt: string;
+  extraClasses?: string | Array<string>;
+  title?: string;
+  url: string;
+}>();
+
+let _atom_class = "a-image";
+</script>
+
+<template>
+  <img
+    :src="url"
+    :class="[_atom_class, extraClasses]"
+    :alt="alt"
+    :title="title ? title : alt"
+  />
+</template>
+
+<style scoped lang="scss">
+@import "./_image";
+</style>
