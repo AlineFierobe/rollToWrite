b78e750463a459cada6cf7ffdb2557c2fff69885
Aowlyne <aowlyne@gmail.com>
2023-12-18 18:42:52 -0500

    update home page

--- src/store/pages/home/homePage.store.ts
+++ src/store/pages/home/homePage.store.ts
@@ -1,6 +1,14 @@
 const homePage = {
-  title: "Roll To Write",
-  description: "Lorem ipsum",
+  banner: {
+    imageUrl:
+      "https://images.unsplash.com/photo-1592819695396-064b9572a660?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Photo de Yannick Pulver sur Unsplash",
+  },
+  description: {
+    title: "Bienvenue sur <span>Roll To Write</span>",
+    icon: "pen-nib",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit dui dictum nisl malesuada rutrum. Integer elementum, enim vitae consectetur luctus, nisi nisi viverra turpis, et hendrerit est elit quis tellus. Nulla eleifend lacinia sapien eu ornare. Nam pulvinar sapien nec purus vehicula lobortis. Nulla vel feugiat dui, ac rhoncus urna. Donec suscipit cursus ex vitae ornare. Maecenas mollis posuere imperdiet. Nulla facilisi. Praesent sed magna non dui aliquet accumsan bibendum sed turpis. Donec feugiat magna nec sapien faucibus, ac rhoncus nisl finibus. Curabitur pretium iaculis nisl ut aliquet. Cras dolor augue, luctus in libero quis, sodales tincidunt tellus. Nam neque justo, tempor sit amet mauris in, porttitor tincidunt eros. Proin et risus orci.",
+  },
 };
 
 export default homePage;
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,9 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BannerAlpha from "@/components/molecules/banner/bannerAlpha/MoleculeBannerAlpha.vue";
+import FontAwesome from "@/components/atoms/fontAwesome/AtomFontAwesome.vue";
+import Typography from "@/components/atoms/typography/AtomTypography.vue";
+
+import data from "@/store/pages/home/homePage.store";
+
+let _page_class = "p-home";
+</script>
 
 <template>
-  <div>Home</div>
+  <div :class="_page_class">
+    <BannerAlpha
+      :alt="data.banner.imageAlt"
+      :extraClasses="_page_class + '__banner'"
+      :url="data.banner.imageUrl"
+    />
+
+    <section :class="_page_class + '__description'">
+      <Typography
+        :extraClasses="_page_class + '__description__title'"
+        :html="data.description.title"
+        htmlTag="h1"
+        version="heading2"
+      />
+
+      <FontAwesome
+        :extraClasses="_page_class + '__description__icon'"
+        :icon="data.description.icon"
+        size="5x"
+      />
+      <Typography
+        :extraClasses="_page_class + '__description__html'"
+        :html="data.description.text"
+      />
+    </section>
+  </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 @import "./_home";
 </style>
