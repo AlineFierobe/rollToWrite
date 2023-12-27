b4ebe98a63839a5f69baf97f8433b20600f28f57
Aowlyne <aowlyne@gmail.com>
2023-12-26 20:29:15 -0500

    update listing view

--- src/store/listings/listing/listing.store.ts
+++ src/store/listings/listing/listing.store.ts
@@ -2,8 +2,10 @@ const listing = {
   title: "Toutes les possibilités",
   description:
     "Tu trouveras ici tout ce qu'il est possible de trouver en résultat des lancers de dés.",
-  defaultText:
+  defaultTagline:
     "Click sur une catégorie pour découvrir toutes les possibilités reliées.",
+  defaultText:
+    "Si tu souhaite ajouter des possibilités, tu peux remplir le formulaire en suivant",
 };
 
 export default listing;
--- src/views/listing/readView/ReadView.vue
+++ src/views/listing/readView/ReadView.vue
@@ -8,16 +8,33 @@ export default {
 import store from "@/store";
 
 import Typography from "@/components/atoms/typography/AtomTypography.vue";
+import NavLink from "@/components/atoms/navLink/AtomNavLink.vue";
 
 let _page_class = "p-listingView";
 </script>
 
 <template>
-  <Typography
-    :extraClasses="_page_class + '__description'"
-    :html="store.listings.listing.defaultText"
-    htmlTag="div"
-  />
+  <div :class="_page_class">
+    <Typography
+      :extraClasses="_page_class + '__tagline'"
+      :html="store.listings.listing.defaultTagline"
+      htmlTag="div"
+      version="subheading2"
+    />
+
+    <div :class="_page_class + '__html'">
+      à
+      <Typography
+        :extraClasses="_page_class + '__text'"
+        :html="store.listings.listing.defaultText"
+        htmlTag="div"
+      />
+
+      <NavLink url="/contact" text="ce lien" />
+    </div>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "readView.scss";
+</style>
